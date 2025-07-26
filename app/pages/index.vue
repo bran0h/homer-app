c4421d480a0ba03c144ca5520ccc16a3e13849e0
Branislav Hozza <brankohozza@gmail.com>
2025-07-26 12:51:36 +0200

    feat: transform to grid

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex gap-x-4">
+  <div class="grid grid-cols-1 md:grid-cols-2 gap-4">
     <ClientOnly>
       <DashboardWeatherCard class="flex-1" />
       <template #fallback>
